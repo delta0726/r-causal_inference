6c959d05cfd39f69ae9a25a9770780051e9df5e6
Keido Sumi <delta0726@nta.pial.jp>
2022-06-10 08:16:57 +0900

    Update Chap12

--- book/causality/chap12_傾向スコアによる層別解析法と重み付け法.R
+++ book/causality/chap12_傾向スコアによる層別解析法と重み付け法.R
@@ -8,8 +8,9 @@
 
 
 # ＜概要＞
-# - 単純な傾向スコアマッチングではATEを推定することはできない
-#   --- 傾向スコアを用いた層別解析法と重み付け法により推定を試みる
+# - 傾向スコアマッチングではATTは推定できるが、ATEは推定することはできない
+# - 傾向スコアのフレキシビリティを享受してATEを算出したい
+#   --- 層別解析法と重み付け法により推定可能
 
 
 # ＜目次＞
@@ -37,6 +38,10 @@ data11 <- read_csv("csv/data11.csv")
 
 # 1 データセットの概要 ---------------------------------------------------------
 
+# ＜ポイント＞
+# - 本章の目的はATEの算出なので、潜在的結果変数から算出されるATEの真値を確認しておく
+
+
 # データ確認
 data11 %>% print()
 
@@ -49,24 +54,23 @@ mean(data11$y1t) - mean(data11$y0t)
 
 # ＜ポイント＞
 # - 層化解析はマッチング方法の議論であるのでmethod引数で"subclass"を選択することにより実現することができる
-# - 推定対象はATEなのでestimate引数により設定する
-
-
-# パラメータ設定
-# --- 層数
-sub <- 5
+#   --- method = "subclass" / estimand = "ATE" と指定する
 
 # モデル構築
 m.out2 <- matchit(t1 ~ x1 + x2 + x3 + x4 + x5 + x6, data = data11,
-                  distance = "glm", method = "subclass", subclass = sub,
+                  distance = "glm", method = "subclass", subclass = 5,
                   estimand = "ATE", min.n = 2)
 
 # マッチングデータの抽出
+# --- マッチングデータにsubclassの列が追加される
+# --- モデル
 m.data2 <- m.out2 %>% match.data()
 m.data2 %>% print()
 
 
 # 3 傾向スコアマッチングの層化解析 ------------------------------------------------
+
+
 
 # オブジェクト生成
 # --- t1の推定値
@@ -82,7 +86,7 @@ robustvar = NULL
 # --- t1のEstimateとStd.Errorを取得
 # --- クラスターに頑健な標準誤差の取得
 j = 1
-for (j in 1:sub) {
+for (j in 1:m.out2$info$subclass) {
   dataps <- m.data2[m.data2$subclass == j,]
   model4 <- lm(y3 ~ t1 + x1 + x2 + x3 + x4 + x5 + x6, data = dataps)
   psp[j] <- model4 %>% summary() %>% use_series(coefficients) %>% .[2, 1]
@@ -92,12 +96,24 @@ for (j in 1:sub) {
 }
 
 
-# クラスターに頑健な標準誤差
+# ウエイトの算出
+# --- 全体のサンプル数
+# --- 層別のサンプル数から算出したウエイト
 n1 <- data11 %>% nrow()
-tauhat <- sum((nps / n1) * psp)
-vartau <- sum((nps / n1)^2 * psvar)
+wgt1 <- nps / n1
+
+# ATEの算出
+# --- 各層内での平均処置効果(psp)をウエイト加重することにより算出
+# --- 3.749（真値は3.756）
+# --- かなり正確に推定することができている
+tauhat <- sum(wgt1 * psp)
+
+# 標準誤差
+vartau <- sum(wgt1^2 * psvar)
 setau <- vartau %>% sqrt()
-robustvartau <- sum((nps / n1)^2 * robustvar)
+
+# クラスターに頑健な標準誤差
+robustvartau <- sum(wgt1^2 * robustvar)
 robustsetau <-robustvartau %>%  sqrt()
 
 # 信頼区間
@@ -108,16 +124,23 @@ tauhat - tstar * robustsetau
 
 # 4 傾向スコアのバランシング評価 ------------------------------------------------------
 
+# ＜ポイント＞
+# - ラブプロットで共変量の分布が処置群と統制群で類似しているかを視覚的に確認
+#   --- サマリーのStd.Mean Diff / Var.Ratioを用いると定量的に評価することができる
+
 # サマリー
 m.out2 %>% summary()
+
+# ラブプロットの作成
+# ---- ライブラリ
+m.out2 %>% summary() %>% plot()
 
 # ラブプロットの作成
 # ---- 手動
 diff1 <- m.out2 %>% summary() %>% use_series(sum.all) %>% .[, 3] %>% abs() %>% rev()
 diff2 <- m.out2 %>% summary() %>% use_series(sum.across) %>% .[, 3] %>% abs() %>% rev()
 maxx <- max(diff1, diff2)
-labels0 <- m.out2 %>% summary() %>% use_series(sum.all) %>% .[, 3] %>% names()
-labels1 <- labels0 %>% rev()
+labels1 <- m.out2 %>% summary() %>% use_series(sum.all) %>% .[, 3] %>% names() %>% rev()
 dotchart(diff1, xlim = c(0, maxx), labels = labels1)
 abline(v = 0.00, col = 8)
 abline(v = 0.10, col = 8)
@@ -125,12 +148,19 @@ abline(v = 0.05, col = 8, lty = 2)
 par(new = TRUE)
 dotchart(diff2, xlim = c(0, maxx), labels = labels1, pch = 16)
 
-# ラブプロットの作成
-# ---- ライブラリ
-m.out2 %>% summary() %>% plot()
-
 
 # 5 傾向スコアによる重み付け法 -------------------------------------------------------
+
+# ＜ポイント＞
+# - 重み付け法は傾向スコアの逆数をウエイトとしてモデルを構築する（加重ウエイトの重回帰分析）
+# - マッチングデータに共変量の偏りが残っている可能性を考慮して、モデルは交絡変数ありで構築する方が良い
+
+
+# ＜欠点＞
+# - 重み付け法は傾向スコアが極小の場合に大きなウエイトを与えてしまうことがある
+#   --- 傾向スコアの算出にエラーがあって極小値が出る場合は問題となる
+#   --- 傾向スコアの予測値が正確に計算されていることが前提
+
 
 # モデル構築
 model1 <- glm(t1 ~ x1 + x2 + x3 + x4 + x5 + x6, data = data11, family = binomial(link = "logit"))
@@ -139,20 +169,21 @@ model1 <- glm(t1 ~ x1 + x2 + x3 + x4 + x5 + x6, data = data11, family = binomial
 ps1 <- model1$fitted.values
 
 # ウエイトの算出
-# --- 傾向スコアの逆数で加重
-if1 <- data11$t1 == 1
-if0 <- data11$t1 == 0
-weights1 <- NULL
-weights1[if1] <- 1 / ps1[if1]
-weights1[if0] <- 1 / (1 - ps1[if0])
+# --- 傾向スコアの逆数（t1が1/0で計算が異なる点に注意）
+weights1 <-
+  data11 %>%
+    select(t1) %>% mutate(weights1 = ifelse(t1 == 1, 1 / ps1, 1 / (1 - ps1))) %>%
+    pull(weights1)
 
 # モデル構築
 # --- 交絡変数なし
-# --- 交絡変数あり
+# --- 交絡変数あり（推奨）
 model2 <- lm(y3 ~ t1, data = data11, weights = weights1)
 model3 <- lm(y3 ~ t1 + x1 + x2 + x3 + x4 + x5 + x6, data = data11, weights = weights1)
 
 # 結果確認
+# --- 交絡変数なしは3.202、交絡変数ありは3.816（真値は3.756）
+# --- 交絡変数をモデルに加えたほうが良好な結果
 model2 %>% summary() %>% use_series(coefficients)
 model3 %>% summary() %>% use_series(coefficients)
 
